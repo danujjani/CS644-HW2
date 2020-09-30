277248b13e7d22a1db0e3002fd3550a7564b1d84
danujjani <71993999+danujjani@users.noreply.github.com>
2020-09-30 08:58:24 -0400

    Update and rename MissingPokerCards.java to MissingPoker.java

--- MissingPoker.java
+++ MissingPoker.java
@@ -1,6 +1,6 @@
 import java.io.IOException;
 import java.util.ArrayList;
-//import java.util.StringTokenizer;
+
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.io.IntWritable;
@@ -13,41 +13,51 @@ import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
 import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
 import org.apache.hadoop.util.GenericOptionsParser;
 
-public class MissingPokerCards{
-    public static class Map extends Mapper<LongWritable, Text, Text, IntWritable>{
-        public void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException{
+public class MissingPoker
+{
+    public static class Map extends Mapper<LongWritable, Text, Text, IntWritable>
+    {
+        public void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException
+	{
      	  String s = value.toString();
           String[] split = s.split(" ");
 	  context.write(new Text(split[0]), new IntWritable(Integer.parseInt(split[1])));
         }
     }
-    public static class Reduce extends Reducer<Text, IntWritable, Text, IntWritable>{
-        public void reduce(Text key, Iterable<IntWritable> value, Context context)throws IOException, InterruptedException{
-   	  int i = 1,existing_card = 0;
+    public static class Reduce extends Reducer<Text, IntWritable, Text, IntWritable>
+    {
+        public void reduce(Text key, Iterable<IntWritable> value, Context context)throws IOException, InterruptedException
+	{
+   	      int i = 1,existing_card = 0;
           ArrayList<Integer> deck = new ArrayList<Integer>();
 
-          for(i=1;i<=13;++i){
+          for(i=1;i<=13;++i)
+	  {
             deck.add(i);
           }
 
-          for(IntWritable cards : value){
+          for(IntWritable cards : value)
+	  {
             existing_card = cards.get();
-            if(deck.contains(existing_card)){
+            if(deck.contains(existing_card))
+	    {
               deck.remove(deck.indexOf(existing_card));
             }
           }
 
-          for(i=0;i<deck.size();++i){
+          for(i=0;i<deck.size();++i)
+	  {
             context.write(key, new IntWritable(deck.get(i)));
           }
         }
       }
-	  public static void main(String[] args) throws Exception {
+	  public static void main(String[] args) throws Exception 
+	  {
             Configuration conf = new Configuration();
             
 
-	    Job job = new Job(conf, "MissingPokerCards");
-    	    job.setJarByClass(MissingPokerCards.class);
+	    Job job = new Job(conf, "MissingPoker");
+    	    job.setJarByClass(MissingPoker.class);
     	    job.setMapperClass(Map.class);
     	    //job.setCombinerClass(Reduce.class);
     	    job.setReducerClass(Reduce.class);
@@ -58,5 +68,5 @@ public class MissingPokerCards{
             FileOutputFormat.setOutputPath(job, new Path(args[1]));
 		  
             System.exit(job.waitForCompletion(true) ? 0 : 1);
-        }
+          }
     }
